730bc06145d40ec1d35cdb509b4a6bfa24d560b6
栗栖 みいお <miio@GGJSapSamba.local>
2011-06-15 02:39:30 +0900

    modify pip target urls.beccause modify github user account alisue to lambdalisue.

--- utils/package.sh
+++ utils/package.sh
@@ -6,7 +6,7 @@ yes | pip install markdown whoosh Pygments PyYAML Akismet
 yes | pip install django django-compress django-reversetag django-pagination
 yes | pip install django-tagging
 # Install from github
-yes | pip install -e git://github.com/alisue/django-modify-history#egg=django-modify-history
-yes | pip install -e git://github.com/alisue/django-markitup-widget#egg=django-markitup-widget
-yes | pip install -e git://github.com/alisue/django-codemirror-widget#egg=django-codemirror-widget
-yes | pip install -e git://github.com/alisue/django-qwert#egg=django-qwert
+yes | pip install -e git://github.com/lambdalisue/django-modify-history#egg=django-modify-history
+yes | pip install -e git://github.com/lambdalisue/django-markitup-widget#egg=django-markitup-widget
+yes | pip install -e git://github.com/lambdalisue/django-codemirror-widget#egg=django-codemirror-widget
+yes | pip install -e git://github.com/lambdalisue/django-qwert#egg=django-qwert
